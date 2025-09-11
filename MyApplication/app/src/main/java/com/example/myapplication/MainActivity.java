9c142e27086f4742ec1acc094f8a5c08f88ffe6b
Bernabe Kondrak <b.kondrak45@gmail.com>
2025-09-11 14:34:40 -0600

    delete button implementuhhh

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -19,8 +19,8 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
-// make [ublic attribute of ''selected' item
+// Notes:
+// make public attribute of 'selected' item
 // delete listener checks if the seleceted item public attribute is populated
 // if selcted item is clicked public attribute is emptied ( so if delte button is presesed nothing happens)
 
@@ -30,10 +30,9 @@ public class MainActivity extends AppCompatActivity {
     // Declare the variables so that you will be able to reference it later.
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
-    ArrayAdapter<String> selectedCityAdapter;
-
     int selectedItemi = -1;
     long list_content;
+    View selectedItemView;
 
 
     ArrayList<String> dataList;
@@ -42,36 +41,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         cityList = findViewById(R.id.city_list);
-
-
-
         String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
         dataList = new ArrayList<>();
         dataList.addAll(Arrays.asList(cities));
-
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList); // 'adapts' the ArrayList into a ListView
         cityList.setAdapter(cityAdapter); // displays the adapter
-
         cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Reset previous selection (if any)
+                if (selectedItemView != null) {
+                    selectedItemView.setBackgroundColor(Color.WHITE);
+                }
+                // Set new selection
+                //view.setBackgroundColor(Color.BLUE);
+                //selectedItemView = view; // Store the currently selected view
                 if (selectedItemi == position) {
                     selectedItemi = -1;
-                    public void setBackgroundColor() {
-
-                    }
-
-                    System.out.println("debug print statement");
-
-                    cityList.setBackgroundColor(Color.WHITE); // Change an attribute of the object
-                    cityList.setAdapter(cityAdapter); // displays the adapter
+                    // selectedItemView.setBackgroundColor(Color.WHITE);
                 } else {
                     selectedItemi = position;
-                    cityList.setBackgroundColor(Color.BLUE); // Change an attribute of the object
-                    cityList.setAdapter(cityAdapter); // displays the adapter
+                    view.setBackgroundColor(Color.LTGRAY);
+                    selectedItemView = view;
+
                 }
             }
         });
@@ -83,7 +76,6 @@ public class MainActivity extends AppCompatActivity {
                 if (selectedItemi != -1) {
                     String poppedElement = dataList.remove(selectedItemi);
                     selectedItemi = -1;
-
                 }
                 cityList.setAdapter(cityAdapter);
             }
